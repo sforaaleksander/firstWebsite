bd869a2decce6f33dae14d399f92cf61b1635984
Alek <sforaaleksander@gmail.com>
2020-06-29 18:27:18 +0200

    further refactor script.js

--- js/script.js
+++ js/script.js
@@ -4,12 +4,12 @@ function validateEmail(InputText) {
 } 
 
 function fieldsNotNullNorEmpty(InputText1, InputText2, InputText3) {
-    return !("/s".test(InputText1) || "/s".test(InputText2) || "/s".test(InputText3)
+    const whiteSpace = /s+/;
+    return !(whiteSpace.test(InputText1) || whiteSpace.test(InputText2) || whiteSpace.test(InputText3)
             || InputText1==="" || InputText2==="" || InputText3==="");
 }
 
 function validateFormSubmission(Email, Name) {
-    alert("Validating");
     if ((!validateEmail(Email))) {
         alert("Invalid email address format!");
     } else {
